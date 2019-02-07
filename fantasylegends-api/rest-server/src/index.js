4bf32d1b610fb7bc9ce9cf51d5497188d2a0ce70
Samuel Chia <samueldchia@gmail.com>
2019-02-07 15:05:45 -0800

    create rest server index

---
+++ fantasylegends-api/rest-server/src/index.js
@@ -0,0 +1,14 @@
+import http from 'http';
+import App from './config/express';
+import { success } from './lib/log';
+
+const app = App.express;
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, err => {
+  if (err) throw new Error;
+  success(`successfully connected to port ${process.env.PORT}`);
+});
+
+export default server;
