b5656f6836a3c61500abbd995ddf0d40d652d0dd
Samuel Chia <samueldchia@gmail.com>
2019-02-13 15:16:35 -0800

    update playerlist controller with external api request

---
+++ fantasysportslegends-api/rest-server/components/playerlist/playerlistController.js
@@ -0,0 +1,15 @@
+import { success, error } from '../../lib/log';
+import { SR_NBA_KEY } from '../../.env';
+
+import axios from 'axios';
+
+
+export const getPlayerListController = async (req, res) => {
+  try {
+    const { data } = await axios.get(`http://api.sportradar.us/nba/trial/v5/en/seasons/2018/reg/leaders.json?api_key=${SR_NBA_KEY}`);
+    return res.status(200).send(data);
+  } catch (err) {
+    error('getPlayerListController - error= ' err);
+    return res.status(400).send(err);
+  }
+}
