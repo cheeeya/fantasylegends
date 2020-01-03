594b30664db6742d753346d7eed193e388b070dd
Samuel Chia <samueldchia@gmail.com>
2020-01-02 17:38:23 -0800

    add auth route

--- fantasysportslegends-api/rest-server/src/routes/index.js
+++ fantasysportslegends-api/rest-server/src/routes/index.js
@@ -4,6 +4,7 @@ import playerListRouter from '../components/playerList/playerListRouter';
 
 const router = express.Router();
 
+router.use('/auth', authRouter);
 router.use('/playerList', playerListRouter);
 
 export default router;
